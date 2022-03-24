6583fa177178dc462d6d8acb32c4b2ad3d4b1135
Kshreenath <54636917+Kshreenath@users.noreply.github.com>
2022-03-24 17:10:36 +0530

    added and updated code

---
+++ AND_Gate/And_Gate.v
@@ -0,0 +1,8 @@
+module And_Gate
+    (
+    input a,b,          // defining inputs A and B of AND gate 
+    output out          // defining output of AND gate
+    );
+
+assign out = a&b;       // Logic implementation
+endmodule
---
+++ AND_Gate/And_Gate_Test_Bench.v
@@ -0,0 +1,16 @@
+// Verilog Test Bench code for AND gate
+module And_Gate_Test_Bench ;
+reg a;reg b;
+wire out ;
+// Defining unit under test i.e And_gate
+And_Gate uut (.a(a) ,.b(b) ,. out(out ));
+// Assigning all possible states for input A and b
+initial begin
+
+a = 0;b = 0;#100;
+a = 0;b = 1;#100;
+a = 1;b = 0;#100;
+a = 1;b = 1;#100;
+
+end
+endmodule
